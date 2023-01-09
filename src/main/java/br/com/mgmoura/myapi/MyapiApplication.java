5c30a3fe6413d32fcafa210e2776ab985ebe940a
marcelogmoura <mgmoura@gmail.com>
2023-01-09 16:36:07 -0300

    usuario repository criado

--- src/main/java/br/com/mgmoura/myapi/MyapiApplication.java
+++ src/main/java/br/com/mgmoura/myapi/MyapiApplication.java
@@ -1,13 +1,33 @@
 package br.com.mgmoura.myapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.mgmoura.myapi.domain.Usuario;
+import br.com.mgmoura.myapi.repositories.UsuarioRepository;
+
 @SpringBootApplication
-public class MyapiApplication {
+public class MyapiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u1 = new Usuario(1, "ana", "ana@email.com", "123");
+		Usuario u2 = new Usuario(2, "ted", "ted@email.com", "123");
+		
+		usuarioRepository.saveAll(Arrays.asList(u1 ,u2));
+	
+	
 	}
 
 }
---
+++ src/main/java/br/com/mgmoura/myapi/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.mgmoura.myapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mgmoura.myapi.domain.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
